b8ae071440a133208ee3f10cf6f9cdce0a22d025
GabbyPrecious <gabbyprecious2000@gmail.com>
2020-09-24 01:33:00 +0100

    edited old models

--- migrations/models/old_models.py
+++ migrations/models/old_models.py
@@ -43,9 +43,9 @@ from tortoise import Model, fields
 MAX_VERSION_LENGTH = 255
 
 
-class Aerich(Model):
-    version = fields.CharField(max_length=MAX_VERSION_LENGTH)
-    app = fields.CharField(max_length=20)
+# class Aerich(Model):
+#     version = fields.CharField(max_length=MAX_VERSION_LENGTH)
+#     app = fields.CharField(max_length=20)
 
-    class Meta:
-        ordering = ["-id"]
+#     class Meta:
+#         ordering = ["-id"]
