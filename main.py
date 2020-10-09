f0deda58bc4ecf429ad44a677ce9c4e0ad68f293
GabbyPrecious <gabbyprecious2000@gmail.com>
2020-10-09 23:44:36 +0100

    code updated

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ app = FastAPI(title="Simple Blog API Built with FastAPI and Tortoise-ORM")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["http://localhost:8080", "http://localhost:8081"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
