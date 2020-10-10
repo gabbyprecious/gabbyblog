6e4660bfbaa58c6967ccbe87007622bc09407a12
GabbyPrecious <gabbyprecious2000@gmail.com>
2020-10-10 14:50:50 +0100

    code updated

--- src/auth/jwthandler.py
+++ src/auth/jwthandler.py
@@ -70,7 +70,7 @@ async def get_current_user(token: str = Depends(security)):
         headers={"WWW-Authenticate": "Bearer"},
     )
     try:
-        payload = jwt.decode(token.credentials, SECRET_KEY, algorithms=[ALGORITHM])
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
         username: str = payload.get("sub")
         if username is None:
             raise credentials_exception
