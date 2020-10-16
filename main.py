55daffd8c28ba1213af83de32560b5af3fd2071d
GabbyPrecious <gabbyprecious2000@gmail.com>
2020-10-16 13:59:05 +0100

    Added hosted site

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ app = FastAPI(title="Simple Blog API Built with FastAPI and Tortoise-ORM")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:8080", "http://localhost:8081", "https://nifty-hopper-1e9895.netlify.app/"],
+    allow_origins=["http://localhost:8080", "http://localhost:8081", "https://nifty-hopper-1e9895.netlify.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
