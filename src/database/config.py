0728777a22e718280e2885eb4190568f94deef48
GabbyPrecious <gabbyprecious2000@gmail.com>
2021-04-26 10:56:56 +0100

    ssl

--- src/database/config.py
+++ src/database/config.py
@@ -1,5 +1,5 @@
 import os
-
+import re
 # {"default": "postgres://blogger:mypass@127.0.0.1:5432/blog_db"}
 # "default": os.environ.get('DATABASE_URL')
 config = re.match("postgres://(.*?):(.*?)@(.*?)/(.*)", os.environ.get("DATABASE_URL"))
